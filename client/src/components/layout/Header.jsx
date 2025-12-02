4e10459ad2bd711892fc20f95de590586173f9e1
Ujjwal Anand <ujjwalanand678@gmail.com>
2025-12-02 17:06:39 +0530

    header fixed

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -7,7 +7,14 @@ import { FaRegClock } from "react-icons/fa";
 import { FaTruckFast, FaLocationDot } from "react-icons/fa6";
 import { HiMenuAlt3 } from "react-icons/hi";
 import Search from "../common/Search";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo/logo.png";
+import arrival from "../../assets/logo/arrival.png";
+import bangles from "../../assets/logo/bangles.png";
+import Earrings from "../../assets/logo/Earrings.png";
+import gold from "../../assets/logo/gold.png";
+import pendent from "../../assets/logo/pendent.png";
+import rings from "../../assets/logo/rings.png";
+import all from "../../assets/logo/all.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -15,18 +22,18 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="w-full bg-[#0E3B3F] text-white font-[Inter]">
+    <header className="w-full bg-[#1B2D2D] text-white ">
       {/* TOP BAR */}
-      <div className="w-full bg-[#064D4F] text-[11px] py-2 flex justify-between px-6 md:px-8 tracking-wide">
-        <div className="hidden md:flex items-center gap-6">
-          <span className="flex items-center gap-2 font-semibold">
+      <div className="w-full bg-[#004545] text-[11px] py-2 flex justify-between px-6 md:px-8 tracking-wide">
+        <div className="hidden md:flex items-center gap-6 ms-40">
+          <span className="flex items-center gap-2 font-semibold ">
             ORDER TRACKING
             <span className="text-sm opacity-80">
               <FaTruckFast />
             </span>
           </span>
 
-          <span>
+          <span >
             Refer and earn extra discount{" "}
             <a href="#" className="text-blue-200 underline">
               Click here...
@@ -58,18 +65,18 @@ const Header = () => {
 
           {/* LOGO */}
           <div className="flex items-center gap-3 select-none">
-            <img src={logo} alt="Gemlay Logo" className="h-10 opacity-80" />
-            <div className="leading-tight hidden md:block">
-              <p className="text-xl font-semibold opacity-90 tracking-wide">
+            <img src={logo} alt="Gemlay Logo" className="h-[37.45px] w-[45px] opacity-80" />
+            <div className="leading-tight hidden md:block text-center ">
+              <p className="text-[16px] font-semibold opacity-90 tracking-wide mb-1">
                 GEMLAY
               </p>
-              <p className="text-xs opacity-70 -mt-1">Pure Diamond Elegance</p>
+              <p className="text-[5px] opacity-70 -mt-1">Pure Diamond Elegance</p>
             </div>
           </div>
         </div>
 
         {/* CENTER: Desktop Search */}
-        <div className="hidden md:block w-[43%]">
+        <div className="hidden md:block w-[472px] h-9">
           <Search placeholder="Slim Sparkle Diamond Ring" />
         </div>
 
@@ -101,36 +108,36 @@ const Header = () => {
       </div>
 
       {/* BOTTOM NAV (DESKTOP ONLY) */}
-      <nav className="hidden md:flex items-center justify-center gap-10 py-4 border-t border-[#1F4C4C] text-sm text-gray-200 tracking-wide">
+      <nav className="hidden md:flex items-center px-10 justify-center gap-5 py-4 border-t border-[#1F4C4C] text-[12px] text-gray-200 tracking-wide">
         <Link
           to="/new-arrivals"
-          className="px-4 py-1 border border-teal-300 rounded-full text-teal-200 font-medium hover:bg-teal-300/10 transition"
+          className="flex gap-1 px-4 py-1 border border-teal-300 rounded-full text-teal-200 font-medium hover:bg-teal-300/10 transition "
         >
-          New Arrivals
+         <img src={arrival} alt="arrival" className="w-[20px] h-[19px]" /> New Arrivals
         </Link>
 
-        <Link to="/rings" className="hover:text-white transition">
-          Rings
+        <Link to="/rings" className="flex gap-1 hover:text-white transition">
+          <img src={rings} alt="rings" className="w-[17px] h-[17px]" />Rings
         </Link>
 
-        <Link to="/earrings" className="hover:text-white transition">
-          Earrings
+        <Link to="/earrings" className="flex gap-1 hover:text-white transition">
+          <img src={Earrings} alt="Earrings" className="w-[17px] h-[17px]" />Earrings
         </Link>
 
-        <Link to="/pendants" className="hover:text-white transition">
-          Pendants
+        <Link to="/pendants" className="flex gap-1 hover:text-white transition">
+          <img src={pendent} alt="pendent" className="w-[17px] h-[17px]" />Pendants
         </Link>
 
-        <Link to="/bangles-bracelets" className="hover:text-white transition">
-          Bangles And Bracelets
+        <Link to="/bangles-bracelets" className="flex gap-1 hover:text-white transition">
+          <img src={bangles} alt="bangles" className="w-[17px] h-[17px]" />Bangles And Bracelets
         </Link>
 
-        <Link to="/gold-coins-bars" className="hover:text-white transition">
-          Gold Coin & Bars
+        <Link to="/gold-coins-bars" className="flex gap-1 hover:text-white transition">
+          <img src={gold} alt="gold" className="w-[17px] h-[17px]" />Gold Coin & Bars
         </Link>
 
-        <Link to="/all-jewellery" className="hover:text-white transition">
-          All Jewellery
+        <Link to="/all-jewellery" className="flex gap-1 item-center hover:text-white transition">
+          <img src={all} alt="all" className="w-[17px] h-[17px]" />All Jewellery
         </Link>
 
         {/* Right-side items */}
